e208926cb2bd902cbd155d8bca49556207a08195
zimin <zimingy3@outlook.com>
2025-08-30 00:24:31 +0800

    Huffman Tree Continue

--- Coding/C/Huffman_Tree.cpp
+++ Coding/C/Huffman_Tree.cpp
@@ -7,5 +7,26 @@ using namespace std;
 class Huffman_Node {
     public:
     char c;
-
+    int freq;
+    Huffman_Node* left, *right;
+    Huffman_Node(char character, int frequency){
+        c=character;
+        freq=frequency;
+        left=nullptr;
+        right=nullptr;
+    }
 }; 
+int main() {
+    cout<<"Enter a string"<<endl;
+    string s;
+    int n;
+    getline(cin,s);
+    n=s.length();
+    unordered_map<char,int> freq_map;
+    for (char c: s){
+        freq_map[c]++;
+    }
+    for (auto p: freq_map) {
+        cout << p.first << ": " << p.second << endl;
+    }
+}
